ad36b39540115d276c01b11ac341e46c61d01330
Gabriel Oliveira <gabrieloliveira.amaro@estudante.ufjf.br>
2025-09-23 22:12:17 -0300

    :pencil2: creating a react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Disciplina from './Disciplina'
 function App() {
   return (
     <>
       <h1>Hi Barbie</h1>
       <p>Hi Ken</p>
+      <Disciplina></Disciplina>
     </>
   )
 }
